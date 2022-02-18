efe970110f3f4a289f113e9441e2ff6a382f0ee8
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-02-18 17:48:52 +0530

    Create Solution.java

---
+++ Linked List/Reverse a linked list/Solution.java
@@ -0,0 +1,29 @@
+class Solution
+{
+    //Function to reverse a linked list.
+    Node reverseList(Node head)
+    {
+        // code here
+        Node list = head;
+        if(head.next == null) return head;
+        else if(head.next.next==null) {
+            list =  head.next;
+            
+             head.next.next = head;
+             head.next = null; 
+            return list;
+        }
+        Node first = head, second = head.next, third = head.next.next;
+        first.next = null;
+    
+        while(third!=null){
+            second.next = first;
+            first  = second;
+            second = third;
+            third = third.next;
+        }
+        second.next = first;
+        first = second;
+        return first;
+    }
+}

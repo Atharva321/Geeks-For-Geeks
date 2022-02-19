91a444a1c9f2832eaa23c07c5f4fabdc8644a87e
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-02-19 07:57:50 +0530

    Create Solution.java

---
+++ Tree/Postorder Traversal/Solution.java
@@ -0,0 +1,17 @@
+class Tree
+{
+    //Function to return a list containing the postorder traversal of the tree.
+    ArrayList<Integer> list = new ArrayList<>();
+    ArrayList<Integer> postOrder(Node root)
+    {
+       
+       // Your code goes here
+       if(root == null) return list;
+       
+       //list.add(root.data);
+       list = postOrder(root.left);
+       //list.add(root.data);
+       list = postOrder(root.right);
+        list.add(root.data);
+        return list;
+    }

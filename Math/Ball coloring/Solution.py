3ef82ed2ef400972e5e9d5ecc3427d229c48540e
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-09-10 18:35:18 +0530

    Update Solution.py

--- Math/Ball coloring/Solution.py
+++ Math/Ball coloring/Solution.py
@@ -1,1 +1,17 @@
+#User function Template for python3
+class Solution:
+    def noOfWays (self, n):
+        # code here 
+        return n*n+2-n
 
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        n = int(input())
+        
+        ob = Solution()
+        print(ob.noOfWays(n))
+# } Driver Code Ends

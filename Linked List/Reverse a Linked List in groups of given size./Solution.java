e32fdf5af7cbf9126a9e0b6de63691d281855259
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-09-23 20:40:14 +0530

    Create Solution.java

---
+++ Linked List/Reverse a Linked List in groups of given size./Solution.java
@@ -0,0 +1,151 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Refresh
+
+Time (IST)	Status	Lang	Test Cases	Code
+2022-08-13 21:39:31	Correct	java	700 / 700	View
+Java (1.8)
+Average Time:
+30m
+Your Time:
+1m
+
+
+
+
+Custom Input
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize
+{
+    static Node head;
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail =tail.next;
+            }
+            
+            int k = sc.nextInt();
+            Solution ob = new Solution();
+            Node res = ob.reverse(head, k);
+            printList(res);
+            System.out.println();
+        }
+    }
+    
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+}
+
+
+// } Driver Code Ends
+
+
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution
+{
+    public static Node reverse(Node node, int k)
+    {
+        //Your code here
+         if(node ==null) return null;
+       Node c=node;
+       Node p=null;
+       int i=1;
+       while(c!=null && i<=k)
+       {
+           Node next=c.next;
+           c.next=p;
+           p=c;
+           c=next;
+           i++;
+       }
+       node.next=reverse(c,k);
+       return p;
+    }
+}

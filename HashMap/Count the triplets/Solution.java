1ce33b21942d0de0934a5bbfe1545cc621c52151
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-10-26 12:58:18 +0530

    Update Solution.java

--- HashMap/Count the triplets/Solution.java
+++ HashMap/Count the triplets/Solution.java
@@ -1,1 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
 
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            
+            int n = Integer.parseInt(read.readLine());
+            String input[] = read.readLine().split(" ");
+            
+            int arr[] = new int[n];
+            for(int i = 0;i<n;i++){
+                arr[i] = Integer.parseInt(input[i]);
+            }
+            
+            Solution ob = new Solution();
+            System.out.println(ob.countTriplet(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+// Space: O(n)
+// Time:  O(n^2)
+class Solution {
+    int countTriplet(int arr[], int n) {
+        // code here
+        HashSet<Integer> hset=new HashSet<>();
+        for(int i=0;i<n;i++){
+            hset.add(arr[i]);
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int sum=arr[i]+arr[j];
+                if(hset.contains(sum)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}

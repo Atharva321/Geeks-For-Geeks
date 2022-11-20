8ae92248f71960c31c8c961d9b41f4c2ff127f41
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-11-20 15:12:06 +0530

    Update Solution.py

--- Math/LCM Triplet/Solution.py
+++ Math/LCM Triplet/Solution.py
@@ -1,1 +1,27 @@
+#User function Template for python3
 
+class Solution:
+    def lcmTriplets(self,N):
+        #code here
+        if N<=2:
+            return N
+        elif N%2!=0:
+            return (N-1)*N*(N-2)
+        elif N%2==0 and N % 3!=0:
+            return N*(N-1)*(N-3)
+        elif(N%2==0 and N%3==0): 
+            return (N-1)*(N-2)*(N-3);
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+        
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        N = int(input())
+        ob=Solution()
+        print(ob.lcmTriplets(N))
+# } Driver Code Ends

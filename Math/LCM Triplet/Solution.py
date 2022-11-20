bdf8c7f29497848a2fa0a65279faa6f7bc1ad869
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-11-20 15:14:09 +0530

    Update Solution.py

--- Math/LCM Triplet/Solution.py
+++ Math/LCM Triplet/Solution.py
@@ -4,19 +4,19 @@ class Solution:
     def lcmTriplets(self,N):
         #code here
         
-        #Less than equal to 2
+        #Less than equal to 2.
         if N<=2:
             return N
         
-        ##Divisible by 2
+        ##Divisible by 2.
         elif N%2!=0:
             return (N-1)*N*(N-2)
         
-        #Only divisible by 2 and not 3
+        #Only divisible by 2 and not 3.
         elif N%2==0 and N % 3!=0:
             return N*(N-1)*(N-3)
         
-        #Divisible by both 2 and 3
+        #Divisible by both 2 and 3.
         elif(N%2==0 and N%3==0): 
             return (N-1)*(N-2)*(N-3);
 

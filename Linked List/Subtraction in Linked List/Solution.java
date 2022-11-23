451cef4638a877724bd5550a8f76efd7b0f379f3
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-11-23 19:51:34 +0530

    Create Solution.java

---
+++ Linked List/Subtraction in Linked List/Solution.java
@@ -0,0 +1,10 @@
+Java Accepted Solution 250/250 Testcases:
+
+Approach:
+
+Convert the two linked list into Strings num1 and num2
+Remove Leading Zeroes the num1 and num2
+Subtract both the Strings and store that string in answer
+Subtract character by character while handling the carry.  Read  Resource
+Remove Leading Zeroes from the answer.
+Convert the answer string into the LinkedList.

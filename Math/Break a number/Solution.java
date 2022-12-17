c5448141e8a8805e91d269e3e258b3ae4db6b4e9
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-12-17 19:21:04 +0530

    Update Solution.java

--- Math/Break a number/Solution.java
+++ Math/Break a number/Solution.java
@@ -1,1 +1,32 @@
+//{ Driver Code Starts
 
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.waysToBreakNumber(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution{
+    int waysToBreakNumber(int N){
+        // code here
+        int mod= 1000000007;
+        long num = N;
+        return (int)((3*num+((num-1)*(num-2))/2)%mod);
+    }
+}

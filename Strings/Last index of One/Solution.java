d6c94831f49498719f12a6eb25f3c25da9ba95ab
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-01-15 20:58:34 +0530

    Update Solution.java

--- Strings/Last index of One/Solution.java
+++ Strings/Last index of One/Solution.java
@@ -31,6 +31,12 @@ class GFG {
 
 class Solution {
     public int lastIndex(String s) {
-      
+        int n = s.length();
+        for(int i=n-1; i>=0; i--){
+            if(s.charAt(i) == '1'){
+                return i;
+            }
+        }
+        return -1;
     }
 }

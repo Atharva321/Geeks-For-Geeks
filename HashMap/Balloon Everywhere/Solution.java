af7fa4a1ec0d6f11cf2fc6e293089050ac02415f
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-02-10 21:35:25 +0530

    Update Solution.java

--- HashMap/Balloon Everywhere/Solution.java
+++ HashMap/Balloon Everywhere/Solution.java
@@ -1,29 +1,6 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-// Position this line where user code will be pasted.
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out=new PrintWriter(System.out);
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String s = br.readLine().trim();
-            Solution ob = new Solution();
-            int ans = ob.maxInstance(s);
-            out.println(ans);
-        }
-        out.close();
-    }
-}
-// } Driver Code Ends
-
 
 // User function Template for Java
 

5aac3ec6a154d0d59e5d720d400e94a25956c761
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-02-15 22:50:48 +0530

    Update Solution.java

--- Strings/Minimum indexed character/Solution.java
+++ Strings/Minimum indexed character/Solution.java
@@ -1,1 +1,20 @@
+// Time  : O(Max(str, patt)) ~ O(n)
+// Space : O(1)
 
+class Solution
+{
+    //Function to find the minimum indexed character.
+    public static int minIndexChar(String str, String patt)
+    {
+        // Your code here
+        int [] chars = new int [26];
+        for(int i=0; i<patt.length(); i++){
+            chars[patt.charAt(i) - 'a']++;
+        }
+        for(int i=0; i<str.length(); i++){
+            if(chars[str.charAt(i) - 'a']!=0) return i;
+        }
+        
+        return -1;
+    }
+}

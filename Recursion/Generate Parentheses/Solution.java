c48d513b3b7060f129464c04d624c9b617ffdd4d
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-02-15 18:32:47 +0530

    Update Solution.java

--- Recursion/Generate Parentheses/Solution.java
+++ Recursion/Generate Parentheses/Solution.java
@@ -1,1 +1,24 @@
-
+class Solution {
+    
+    List<String> ans = new ArrayList<>();
+    
+    public List<String> generateParenthesis(int n) {
+        parenthesesGenerator(0, 0, "", n);
+        return ans;
+    }
+    
+    public void parenthesesGenerator(int open, int close, String  curr, int n){
+        if(curr.length() == n*2){
+            ans.add(curr);
+            return;
+        }
+        
+        if(open < n){
+            parenthesesGenerator(open+1, close, curr+'(', n);
+        }
+        
+        if(open > close) { 
+            parenthesesGenerator(open, close+1, curr+')', n);
+        }
+    }
+}

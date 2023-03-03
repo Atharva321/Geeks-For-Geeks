ae9115f0dd87318a51195ee3ffa69eb667c3394d
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-03 22:50:33 +0530

    Update Solution.java

--- Queue/Stack using two queues/Solution.java
+++ Queue/Stack using two queues/Solution.java
@@ -1,42 +1,6 @@
 //{ Driver Code Starts
 import java.util.*;
 
-
-class StackUsingQueues
-{
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		while(t>0)
-		{
-			Queues g = new Queues();
-			
-			int q = sc.nextInt();
-			while(q>0)
-			{
-				int QueryType = sc.nextInt();
-				if(QueryType == 1)
-				{
-					int a = sc.nextInt();
-					g.push(a);
-				}
-				else if(QueryType == 2)
-				System.out.print(g.pop()+" ");
-			q--;
-			}	
-			System.out.println();
-				
-			
-			
-		t--;
-		}
-	}
-}
-
-
-// } Driver Code Ends
-
 // queue
 // q.add() 1 2 3
 // q.peek(); 1 2 3

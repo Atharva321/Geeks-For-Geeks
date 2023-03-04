0bfee9fdccfb05e0d6baffe91acd86f2d713fc40
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-04 23:55:34 +0530

    Update Solution.java

--- Stack/Parenthesis Checker/Solution.java
+++ Stack/Parenthesis Checker/Solution.java
@@ -1,1 +1,19 @@
+class Solution
+{
+    //Function to check if brackets are balanced or not.
+    static boolean ispar(String x)
+    {
+        // add your code here
+        Stack<Character> st=new Stack<>();
+         for(int i=0;i<x.length();i++){
 
+                 if((st.empty()==false) && ((x.charAt(i)==')' && st.peek()=='(') || (x.charAt(i)=='}' 
+                         && st.peek()=='{') || (x.charAt(i)==']') && st.peek()=='[')){
+                     st.pop();
+                 }
+             else
+                 st.push(x.charAt(i));
+         }
+         return (st.empty())?true:false;
+    }
+}

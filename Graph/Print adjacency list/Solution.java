e827fb17e0badedefbbd462fbcceea13d1a2b5bd
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-04 23:01:25 +0530

    Update Solution.java

--- Graph/Print adjacency list/Solution.java
+++ Graph/Print adjacency list/Solution.java
@@ -1,1 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+class Solution {
+    // Function to return the adjacency list for each vertex.
+    public ArrayList<ArrayList<Integer>> printGraph(
+        int V, ArrayList<ArrayList<Integer>> graph) {
+        // Code here
+        
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        
+        for(int i=0; i<graph.size(); i++){
+            adj.add(new ArrayList<Integer>());
+        }
+        
+        for(int i=0; i<V; i++){
+            adj.get(i).add(i);
+            for(int nbr: graph.get(i)){
+                adj.get(i).add(nbr);
+            }
+        }
+        
+        return adj;
+    }
+}

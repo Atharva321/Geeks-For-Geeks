682d103b53f794da9426742d61cf9232639381ee
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-05 23:09:30 +0530

    Update Solution.java

--- Greedy/Minimum number of jumps/Solution.java
+++ Greedy/Minimum number of jumps/Solution.java
@@ -1,30 +1,9 @@
-//{ Driver Code Starts
+// Time  : O(n)
+// Space : O(1)
+
 import java.lang.*;
 import java.io.*;
 import java.util.*;
-class GFG
- {
-	public static void main (String[] args) throws IOException
-	 {
-	 
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-        int t = Integer.parseInt(br.readLine()); 
-
-        while(t-- > 0){
-            int size = Integer.parseInt(br.readLine());
-            String[] arrStr = ((String)br.readLine()).split("\\s+");
-            int[] arr= new int[size];
-            for(int i = 0;i<size;i++){
-                arr[i] = Integer.parseInt(arrStr[i]);
-            }
-            System.out.println(new Solution().minJumps(arr));
-        }
-	 }
-	 
-}
-
-// } Driver Code Ends
-
 
 class Solution{
     static int minJumps(int[] arr){

d357359a2fda0fabeaf46c49b9b8f33d50c974b5
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-05 23:06:24 +0530

    Update Solution.java

--- Tree/Next Right Node/Solution.java
+++ Tree/Next Right Node/Solution.java
@@ -1,1 +1,38 @@
-
+class Solution{
+	Node nextRight(Node root, int key)
+    {
+		//Write your code here
+		if(root.data==key){
+            return new Node(-1);
+        }
+        
+        Queue<Node> q =new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                Node top=q.poll();
+                if(top!=null){
+                    if(top.data==key){
+                        if(!q.isEmpty() && q.peek()!=null){
+                            return q.peek();
+                        }
+                        else{
+                            return new Node(-1);
+                        }
+                    }
+                
+                    if(top.left!=null){
+                        q.add(top.left);
+                    }
+                
+                    if(top.right!=null){
+                        q.add(top.right);
+                    }
+                }
+            }
+            q.add(null);
+        }
+        return new Node(-1);
+    }
+}

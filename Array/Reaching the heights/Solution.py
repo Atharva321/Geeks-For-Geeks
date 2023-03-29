84f18be31ffb1786a1375f80b075e82885413c4a
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-29 23:38:11 +0530

    Update Solution.py

--- Array/Reaching the heights/Solution.py
+++ Array/Reaching the heights/Solution.py
@@ -1,1 +1,20 @@
-
+def reaching_height (n, arr) : 
+    #Complete the function
+   if(n==1):
+       return arr
+   arr.sort()
+   i = 0 
+   j = n-1
+   result = [0 for i in range(n)]
+   k = 0
+   while(i<=j):
+       if(k%2==0):
+           result[k] = arr[j]
+           j-=1
+       else:
+           result[k] = arr[i]
+           i+=1
+       k+=1
+   if(result[0] == result[-1]):
+       return [-1]
+   return result

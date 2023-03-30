192bab2704b31a487db548c1c4766aa0588ba043
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-30 23:25:26 +0530

    Update Solution.java

--- HashMap/Find first repeated character/Solution.java
+++ HashMap/Find first repeated character/Solution.java
@@ -1,1 +1,17 @@
-
+class Solution 
+{ 
+    String firstRepChar(String s) 
+    { 
+        // code here
+        int [] charFreqMap = new int [26];
+        Arrays.fill(charFreqMap, 0);
+        
+        for(int i=0; i<s.length(); i++){
+            if(charFreqMap[s.charAt(i) - 'a'] >= 1){
+                return "" + s.charAt(i);
+            }
+            charFreqMap[s.charAt(i) - 'a']++;
+        }
+        return "-1";
+    }
+} 

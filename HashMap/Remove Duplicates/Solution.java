43ec7fcf9a5318d00c76a7038b08a4afc53c704b
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-30 23:34:19 +0530

    Update Solution.java

--- HashMap/Remove Duplicates/Solution.java
+++ HashMap/Remove Duplicates/Solution.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    String removeDups(String s) {
+        // code here
+        int [] charFreqMap = new int [26];
+        Arrays.fill(charFreqMap, 0);
+        String ans = "";
+        
+        for(int i=0; i<s.length(); i++){
+            charFreqMap[s.charAt(i) - 'a']++;
+            if(charFreqMap[s.charAt(i) - 'a'] == 1){
+                ans += s.charAt(i);
+            }
+        }
+        return ans;
+    }
+}

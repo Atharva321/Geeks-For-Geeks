559232572b5a34ea48511910a544b8be89d27444
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-03 22:34:44 +0530

    Update Solution.java

--- Math/Power Of Numbers/Solution.java
+++ Math/Power Of Numbers/Solution.java
@@ -1,1 +1,17 @@
+class Solution
+{
+        
+    long mod = 1000000007;
+    
+    long power(int N,int R)
+    {
+        //Your code here
+        if(R==0) return 1;
+        long p = power(N, R/2);
+        if(R%2==1){
+            return (N*p)%mod*p%mod;
+        }
+        return (p*p)%mod;
+    }
 
+}

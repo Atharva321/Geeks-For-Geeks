db30257531113208de500257ed020a9fccd1b3bf
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-04 22:01:38 +0530

    Update Solution.java

--- Array/Rotate matrix elements clockwise/Solution.java
+++ Array/Rotate matrix elements clockwise/Solution.java
@@ -4,37 +4,6 @@
 import java.io.*;
 import java.util.*;
 
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String s[] = read.readLine().split(" ");
-            int N = Integer.parseInt(s[0]);
-            int M = Integer.parseInt(s[1]);
-            int A[][] = new int[N][M];
-            for (int i = 0; i < N; i++) {
-                String S[] = read.readLine().split(" ");
-                for (int j = 0; j < M; j++) {
-                    A[i][j] = Integer.parseInt(S[j]);
-                }
-            }
-            Solution ob = new Solution();
-            int ans[][] = ob.rotateMatrix(N, M, A);
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) System.out.print(ans[i][j] + " ");
-                System.out.println();
-            }
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     int[][] rotateMatrix(int M, int N, int Mat[][]) {
         // code here

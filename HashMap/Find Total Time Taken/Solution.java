edf009c67323e512ab7aeed5cd87910785f83ad4
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-15 22:11:37 +0530

    Update Solution.java

--- HashMap/Find Total Time Taken/Solution.java
+++ HashMap/Find Total Time Taken/Solution.java
@@ -1,1 +1,20 @@
-
+class Solution
+{
+    public static long totalTime(int n,int arr[],int time[])
+    {
+        HashSet<Integer> hset =  new HashSet<>();
+        int totalTime = 0;
+        hset.add(arr[0]);
+        
+        for(int i=1; i<arr.length; i++){
+            if(hset.contains(arr[i])){
+                totalTime += time[arr[i]-1];
+            }
+            else{
+                totalTime++;
+                hset.add(arr[i]);
+            }
+        }
+        return totalTime;
+    }
+}

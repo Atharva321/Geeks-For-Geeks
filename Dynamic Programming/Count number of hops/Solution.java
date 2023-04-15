a4b29e9b9a8d7684708afafd8e4b32692e4e3d06
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-15 22:43:03 +0530

    Update Solution.java

--- Dynamic Programming/Count number of hops/Solution.java
+++ Dynamic Programming/Count number of hops/Solution.java
@@ -1,1 +1,25 @@
-
+class Solution
+{
+    //Function to count the number of ways in which frog can reach the top.
+    static long countWays(int n)
+    {
+        // add your code here
+        int first = 1;
+        int second = 2;
+        int third = 4;
+        int mod = 1000000007;
+        
+        if(n==1) return first;
+        if(n==2) return second;
+        if(n==3) return third;
+        
+        for(int i=4; i<=n; i++){
+            int curr = ((first + second)%mod + third)%mod;
+            first = second;
+            second = third;
+            third = curr;
+        }
+        return third;
+    }
+    
+}

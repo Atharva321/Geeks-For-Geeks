3faf65509b08f1502c2ba9420787151004844596
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-18 23:51:39 +0530

    Update Solution.java

--- Strings/Anagram of String/Solution.java
+++ Strings/Anagram of String/Solution.java
@@ -1,27 +1,26 @@
 class GfG
 {
-	public int remAnagrams(String str1,String str2)
-        {
+    public int remAnagrams(String str1,String str2)
+    {
         //add code here.
         int[] count1 = new int[26]; int[] count2 = new int[26];
-  
-    for(int i=0;i<str1.length();i++)
-    {
-        count1[str1.charAt(i)-'a']++;
-    }
-    
-     for(int i=0;i<str2.length();i++)
-    {
-        count2[str2.charAt(i)-'a']++;
-    }
-    int chars=0;
-    for(int i=0;i<26;i++)
-    {
-        if(count1[i]!=count2[i])
+        for(int i=0;i<str1.length();i++)
         {
-            chars+= Math.abs(count1[i]- count2[i]);
+            count1[str1.charAt(i)-'a']++;
         }
+
+        for(int i=0;i<str2.length();i++)
+        {
+            count2[str2.charAt(i)-'a']++;
+        }
+        int chars=0;
+        for(int i=0;i<26;i++)
+        {
+            if(count1[i]!=count2[i])
+            {
+                chars+= Math.abs(count1[i]- count2[i]);
+            }
+        }
+        return chars;
     }
-    return chars;
-        }
 }

c60bb1c3a53cc3de929a06020a007ab1e49c06de
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-18 23:46:19 +0530

    Update Solution.java

--- Strings/Anagram of String/Solution.java
+++ Strings/Anagram of String/Solution.java
@@ -1,21 +1,3 @@
-// { Driver Code Starts
-//saksham raj seth
-import java.util.*;
-class Anagrams{
-	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
-		int t=sc.nextInt();
-		while(t-->0){
-			String s=sc.next();
-			String s1=sc.next();
-			GfG g=new GfG();
-			System.out.println(g.remAnagrams(s,s1));
-		}
-	}
-}// } Driver Code Ends
-
-
-/*Complete the function below*/
 class GfG
 {
 	public int remAnagrams(String str1,String str2)

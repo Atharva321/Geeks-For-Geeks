c5d21440141720ce360b3ae90113973c311031c4
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-18 23:45:44 +0530

    Update Solution.py

--- Strings/Reverse words in a given string/Solution.py
+++ Strings/Reverse words in a given string/Solution.py
@@ -9,10 +9,7 @@ class Solution:
         # code here 
         S = ".".join(S.split(".")[::-1])
         return S
-#{ 
- # Driver Code Starts
-# Initial Template for Python 3
-
+    
 if __name__ == '__main__':
     t = int(input())
     for i in range(t):

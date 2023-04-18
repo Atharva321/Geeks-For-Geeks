29e7b0870e9718b0039ba00519ceebd850ca57da
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-18 23:48:31 +0530

    Update Solution.java

--- Math/Fill up buckets/Solution.java
+++ Math/Fill up buckets/Solution.java
@@ -1,1 +1,12 @@
-
+class Solution{
+    public int totalWays(int n, int[] capacity) {
+        // code here
+        Arrays.sort(capacity);
+        int mod=1000000007;
+        long ans=1;
+        for(int i=0;i<n;i++){
+            ans=((ans%mod)*(capacity[i]-i))%mod;
+        }
+        return (int)ans;
+    }
+}

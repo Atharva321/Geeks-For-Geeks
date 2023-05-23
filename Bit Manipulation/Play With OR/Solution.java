8b71d60018b52e9824e8ae5ce6ec2a4ed2ab2788
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-05-23 23:41:33 +0530

    Update Solution.java

--- Bit Manipulation/Play With OR/Solution.java
+++ Bit Manipulation/Play With OR/Solution.java
@@ -3,7 +3,7 @@ class Complete{
     public static int[] game_with_number (int arr[], int n) {
         // Complete the function
         for(int i=0; i< n-1; i++){
-            arr[i]|=arr[i+1];
+            arr[i] |= arr[i+1];
         }
         return arr;
     }

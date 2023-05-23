39cc425e31ec32bcf68b505f0414afac7b9ba26e
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-05-23 23:57:12 +0530

    Update Solution.java

--- Math/Middle of Three/Solution.java
+++ Math/Middle of Three/Solution.java
@@ -1,1 +1,10 @@
-
+class Solution{
+    int middle(int A, int B, int C){
+        //code here
+        int max = Math.max(A,Math.max(B,C));
+        int min = Math.min(A,Math.min(B,C));
+        
+        int ans = A + B + C - (max + min);
+        return ans;
+    }
+}

7312dedff2c3990b50d1c9c3edbb5988ce53a02c
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-05-28 23:52:16 +0530

    Update Solution.py

--- Linked List/Nth node from end of linked list/Solution.py
+++ Linked List/Nth node from end of linked list/Solution.py
@@ -1,1 +1,18 @@
+#User function Template for python3
+'''
+	Your task is to return the data stored in
+	the nth node from end of linked list.
+	
+	Function Arguments: head (reference to head of the list), n (pos of node from end)
+	Return Type: Integer or -1 if no such node exits.
 
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+'''
+#Function to find the data of nth node from the end of a linked list
+def getNthFromLast(head,n):

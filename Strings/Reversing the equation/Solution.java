afd29bc7d29bf351d90e5944f881c73798668ef9
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-06-04 23:49:50 +0530

    Update Solution.java

--- Strings/Reversing the equation/Solution.java
+++ Strings/Reversing the equation/Solution.java
@@ -1,4 +1,27 @@
+// Time  : O(n)
+// Space : O(n)
+
 Class Solution
 {
+    String reverseEqn(String S)
+    {
+        // your code here
+        StringBuilder ans = new StringBuilder();
+        StringBuilder num = new StringBuilder();
 
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+
+            if (Character.isDigit(c)) {
+                num.insert(0,c);
+            } else {
+                ans.append(num);
+                ans.append(c);
+                num.setLength(0);
+            }
+        }
+
+        ans.append(num);
+        return ans.reverse().toString();
+    }
 }

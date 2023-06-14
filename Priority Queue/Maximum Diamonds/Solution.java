910ff91cd4ca0f08a67665443cd6bab126083764
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-06-14 23:45:10 +0530

    Update Solution.java

--- Priority Queue/Maximum Diamonds/Solution.java
+++ Priority Queue/Maximum Diamonds/Solution.java
@@ -1,6 +1,25 @@
+// Time  : O(nlogn)
+// Space : O(n)
+
 class Solution {
     static long maxDiamonds(int[] A, int N, int K) {
         // code here
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
+        long diamonds = 0;
+        
+        for(int i=0; i<A.length; i++){
+            pq.add(A[i]);
+        }
+        
+        while(!pq.isEmpty() && K>0){
+            int mostDiamonds = pq.poll();
+            diamonds = diamonds + mostDiamonds;
+            mostDiamonds = mostDiamonds/2;
+            pq.add(mostDiamonds);
+            K--;
+        }
+        
+        return diamonds;
     }
 };

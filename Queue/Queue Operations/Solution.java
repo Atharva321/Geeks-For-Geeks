beba0ecc1ab60620543c07f4a04b00affa065330
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-06-17 23:56:04 +0530

    Update Solution.java

--- Queue/Queue Operations/Solution.java
+++ Queue/Queue Operations/Solution.java
@@ -1,2 +1,24 @@
-// Time  :
-// Space :
+// Time  : O(n)
+// Space : O(n)
+
+class Geeks{
+    // Function to insert element into the queue
+    static void insert(Queue<Integer> q, int k){
+        
+        // Your code here
+        q.add(k);
+    }
+    
+    // Function to find frequency of an element
+    // rteturn the frequency of k
+    static int findFrequency(Queue<Integer> q, int k){
+        
+        // Your code here
+        int freq = 0;
+        for(Integer num: q){
+            if(num == k) freq++;
+        }
+        return freq;
+    }
+    
+}

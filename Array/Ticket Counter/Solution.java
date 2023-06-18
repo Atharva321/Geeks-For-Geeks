3f9eeed0d5b975c3bcd37dc4035b2b2537b7237a
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-06-18 23:07:13 +0530

    Update Solution.java

--- Array/Ticket Counter/Solution.java
+++ Array/Ticket Counter/Solution.java
@@ -1,7 +1,28 @@
+// Time  : O(n)
+// Space : O(1)
+
 class Solution {
     public static int distributeTicket(int N,int K)
     {
       // code here
-        
+        int left=1, right=N;
+        int lastPerson = left;
+        while(left<=right){
+            if(left+K < right){
+                left = left + K;
+            }
+            else{
+                lastPerson = right;
+                break;
+            }
+            if(left < right-K){
+                right = right-K;
+            }
+            else{
+                lastPerson = left;
+                break;
+            }
+        }
+        return lastPerson;
     }
 }

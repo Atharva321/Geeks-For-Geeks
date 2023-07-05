575122e3ccb09529b566a12b35f36a6d4dc2ad02
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-07-05 23:47:13 +0530

    Update Solution.java

--- Array/Stock buy and sell II/Solution.java
+++ Array/Stock buy and sell II/Solution.java
@@ -1,1 +1,17 @@
+// Time  : O(n)
+// Space : O(1)
 
+class Solution {
+    public static int stockBuyAndSell(int n, int[] prices) {
+        // code here
+        
+        int profit = 0;
+        for (int i = 1; i < n; i++) {
+            int currProfit = 0;
+            if (prices[i] > prices[i - 1])
+                currProfit = prices[i] - prices[i - 1];
+                profit = profit + currProfit;
+        }
+        return profit;
+    }
+}

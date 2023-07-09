306034a1e6ebd71e31bc0f92a17354306a50421d
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-07-09 18:00:33 +0530

    Update Solution.java

--- Array/Smallest Positive missing number/Solution.java
+++ Array/Smallest Positive missing number/Solution.java
@@ -1,1 +1,24 @@
+// Time  : O(n)
+// Space : O(1)
 
+class Solution
+{
+    //Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int size)
+    {
+        // Your code here
+        int [] isPresent = new int [1000001];
+        for(int i=0; i < size; i++){
+            if(arr[i] > 0){
+                isPresent[arr[i]]++;
+            }
+        }
+        
+        for(int i=1; i<isPresent.length; i++){
+            if(isPresent[i] == 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+}

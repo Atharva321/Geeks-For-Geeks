7541ea22eb24ff8650b22c62df3660b82b13c5d3
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-07-13 22:18:10 +0530

    Update Solution.java

--- HashMap/Unique Number of Occurrences/Solution.java
+++ HashMap/Unique Number of Occurrences/Solution.java
@@ -1,1 +1,25 @@
+// Time  : O(n)
+// Space : O(n)
 
+class Solution {
+    public static boolean isFrequencyUnique(int n, int[] arr) {
+        // code here
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        for(int num: arr){
+            hmap.put(num, hmap.getOrDefault(num, 0)+1);
+        }
+        
+        HashSet<Integer> hset = new HashSet<>();
+        
+        for(int value : hmap.values()){
+            if(hset.contains(value)){
+                return false;
+            }
+            else{
+                hset.add(value);
+            }
+        }
+        
+        return true;
+    }
+}

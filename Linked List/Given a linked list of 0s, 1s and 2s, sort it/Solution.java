b0fce2d06dd952090cb62a0f802781f37ebe0051
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-07-23 22:55:41 +0530

    Update Solution.java

--- Linked List/Given a linked list of 0s, 1s and 2s, sort it/Solution.java
+++ Linked List/Given a linked list of 0s, 1s and 2s, sort it/Solution.java
@@ -1,1 +1,39 @@
-
+class Solution
+{
+    //Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head)
+    {
+        // add your code here
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+        Node temp = head;
+        while(temp != null){
+            if(temp.data == 0) zero++;
+            if(temp.data == 1)  one++;
+            if(temp.data == 2)  two++;
+            temp = temp.next;
+        }
+        
+        temp = head;
+        while(temp != null && zero != 0){
+            temp.data = 0;
+            temp = temp.next;
+            zero--;
+        }
+        
+        while(temp != null && one != 0){
+            temp.data = 1;
+            temp = temp.next;
+            one--;
+        }
+        
+        while(temp != null && two != 0){
+            temp.data = 2;
+            temp = temp.next;
+            two--;
+        }
+        
+        return head;
+    }
+}

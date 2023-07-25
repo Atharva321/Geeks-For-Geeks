ef8bec3365567ae4a2df208290c68ce176ccc862
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-07-25 23:57:27 +0530

    Update Solution_Latest.java

--- Tree/Level order traversal in spiral form/Solution_Latest.java
+++ Tree/Level order traversal in spiral form/Solution_Latest.java
@@ -1,1 +1,37 @@
-
+class Spiral
+{
+    //Function to return a list containing the level order 
+    //traversal in spiral form.	
+    ArrayList<Integer> findSpiral(Node root) 
+    {
+        // Your code here
+        int nodes;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        ArrayList<Integer> spiralTraversal = new ArrayList<>();
+        int level = 0;
+        while(!q.isEmpty()){
+            nodes = q.size();
+            ArrayList<Integer> levelNodes = new ArrayList<>();
+            
+            for(int i=0; i<nodes; i++){
+                Node node = q.poll();
+                levelNodes.add(node.data);
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+            }
+            
+            if(level%2 != 0){
+                spiralTraversal.addAll((levelNodes));
+            }
+            else{
+                Collections.reverse(levelNodes);
+                spiralTraversal.addAll(levelNodes);
+            }
+            
+            level++;
+        }
+        
+        return spiralTraversal;
+    }
+}

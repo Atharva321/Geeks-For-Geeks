fe2cf9e2122b02f6a6f5cf87b0af22a33a374517
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-07-31 22:01:37 +0530

    Update Solution_BFS.java

--- Graph/BFS of graph/Solution_BFS.java
+++ Graph/BFS of graph/Solution_BFS.java
@@ -1,1 +1,26 @@
+// Time  : O(V + E)
+// Space : O(V + E)
 
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> bfsTraversal = new ArrayList<>();
+        boolean [] isVisited = new boolean[V];
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        while(!q.isEmpty()){
+            int currVertice = q.poll();
+            bfsTraversal.add(currVertice);
+            for(int vertice: adj.get(currVertice)){
+                if(isVisited[vertice] == false){   
+                    isVisited[vertice] = true;
+                    q.add(vertice);
+                }
+            }
+        }
+        
+        return bfsTraversal;
+    }
+}

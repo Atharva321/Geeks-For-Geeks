278e3a3e04d73eca343e0e8748f39105b7d425ae
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-08-04 23:59:25 +0530

    Update Solution.java

--- Queue/Reverse a Stack/Solution.java
+++ Queue/Reverse a Stack/Solution.java
@@ -1,1 +1,19 @@
+// Time  : O(n)
+// Space : O(n)
 
+class Solution
+{ 
+    static void reverse(Stack<Integer> st)
+    {
+        // add your code here
+        Queue<Integer> q = new LinkedList<>();
+        
+        while(!st.isEmpty()){
+            q.add(st.pop());
+        }
+        
+        while(!q.isEmpty()){
+            st.push(q.poll());
+        }
+    }
+}

83b008e39fb47c93aac50fec9073e685747891a6
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-08-05 23:56:25 +0530

    Update Solution.java

--- Array/Chocolate Distribution Problem/Solution.java
+++ Array/Chocolate Distribution Problem/Solution.java
@@ -1,1 +1,16 @@
-
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        // your code here
+        Collections.sort(a);
+        long diff = (long)Integer.MAX_VALUE;
+        int i=0;
+        while(i+m <= n){
+            diff = Math.min(diff,(long) a.get(i+m-1) - a.get(i));
+            // System.out.println(diff);
+            i++;
+        }
+        return diff;
+    }
+}

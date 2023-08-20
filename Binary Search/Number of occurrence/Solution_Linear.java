df4312c09c0e825b5b32bad910005427dc5e63a5
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-08-20 23:22:41 +0530

    Update Solution_Linear.java

--- Binary Search/Number of occurrence/Solution_Linear.java
+++ Binary Search/Number of occurrence/Solution_Linear.java
@@ -1,1 +1,14 @@
+// Time  : O(n)
+// Space : O(1)
 
+class Solution {
+    int count(int[] arr, int n, int x) {
+        // code here
+        int freq = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i] == x) freq++;
+        }
+        
+        return freq;
+    }
+}

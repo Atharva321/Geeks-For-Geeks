8f58dbd71957f1f690fb0219ba56e3217a3ec34d
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-08-29 23:56:30 +0530

    Update Solution.java

--- Linked List/Delete nodes having greater value on right/Solution.java
+++ Linked List/Delete nodes having greater value on right/Solution.java
@@ -1,1 +1,45 @@
-
+class Solution {
+    public Node reverse(Node head) {
+        Node current = head;
+        Node previous = null;
+        
+        while (current != null) {
+            Node temp = current.next;
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
+        
+        return previous;
+    }
+    
+    public Node filterGreaterNodes(Node head) {
+        Node current = head;
+        Node filteredHead = null;
+        Node filteredTail = null;
+        
+        while (current != null) {
+            if (filteredHead == null || current.data >= filteredTail.data) {
+                if (filteredHead == null) {
+                    filteredHead = current;
+                } else {
+                    filteredTail.next = current;
+                }
+                filteredTail = current;
+            }
+            current = current.next;
+        }
+        
+        if (filteredTail != null) {
+            filteredTail.next = null;
+        }
+        
+        return filteredHead;
+    }
+    
+    public Node compute(Node head) {
+        Node reversedHead = reverse(head);
+        Node filteredHead = filterGreaterNodes(reversedHead);
+        return reverse(filteredHead);
+    }
+}

39d321cb0c0fe32aa9bd8c39914088df9a0e84c8
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-09-15 23:58:52 +0530

    Update Solution_Recursion.java

--- Recursion/Partition Equal Subset Sum/Solution_Recursion.java
+++ Recursion/Partition Equal Subset Sum/Solution_Recursion.java
@@ -1,1 +1,28 @@
+class Solution{
+    
+    public static boolean isPossibleToPartitionEqually(int targetSum, int[] numbers, int currentIndex) {
+        if (targetSum <= 0 || currentIndex < 0) {
+            return targetSum == 0;
+        }
+        return isPossibleToPartitionEqually(targetSum - numbers[currentIndex], numbers, currentIndex - 1)
+            || isPossibleToPartitionEqually(targetSum, numbers, currentIndex - 1);
+    }
 
+
+    static int equalPartition(int N, int numbers[])
+    {
+        // code here
+        int totalSum = 0;
+        for (int number : numbers) {
+            totalSum += number;
+        }
+
+        if (totalSum % 2 != 0) {
+            return 0;
+        }
+
+        int targetSum = totalSum / 2;
+        if(isPossibleToPartitionEqually(targetSum, numbers, numbers.length - 1)== true) return 1; 
+        return 0;
+    }
+}

cb1eee585366e788c7c1c3548fdca72374c9b3fa
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-10-25 00:12:15 +0530

    Update and rename Solution.java to Solution_BFS.java

---
+++ Tree/Reverse Level Order Traversal/Solution_BFS.java
@@ -0,0 +1,33 @@
+class Tree
+{
+    public ArrayList<Integer> reverseLevelOrder(Node root) 
+    {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        if(root == null) return ans;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        int max;
+        while(!q.isEmpty()){
+            int size = q.size();
+            ArrayList<Integer> currNodes = new ArrayList<>();
+            
+            max = Integer.MIN_VALUE;
+            
+            for(int i=0; i<size; i++){
+                Node curr = q.poll();
+                currNodes.add(curr.data);
+                
+                if(curr.left!= null)   q.add(curr.left);
+                if(curr.right!= null)  q.add(curr.right);
+            }
+      
+            Collections.reverse(currNodes);
+            ans.addAll(currNodes);
+        }
+        
+        Collections.reverse(ans);
+        return ans;
+    }
+}      

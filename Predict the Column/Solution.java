bcf2c3c04911de3a0af59fa000bab1220fbf947f
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-11-09 22:25:40 +0530

    Update Solution.java

--- Predict the Column/Solution.java
+++ Predict the Column/Solution.java
@@ -1,1 +1,25 @@
-
+class Solution
+{
+    int columnWithMaxZeros(int arr[][], int N)
+    {
+        // code here 
+        int index = -1;
+        int minSum = Integer.MAX_VALUE;
+        
+        for(int  col = 0; col < arr[0].length; col++){
+            int row = 0;
+            int sum = 0;
+            while(row < arr.length){
+                sum = sum + arr[row][col];
+                row++;
+            }
+            if(minSum > sum){
+                minSum = sum;
+                if(sum < arr.length)
+                index = col;
+            }
+        }
+        
+        return index;
+    }
+}

527d9604d25aa150dbbfb18b317ea109cfe1b7da
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-12-01 23:21:01 +0530

    Create Solution.java

---
+++ Tree/1662. Check If Two String Arrays are Equivalent/Solution.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    
+    public static boolean isDeadEnd(Node root)
+    {
+        //Add your code here.
+        
+        return inorder(1, Integer.MAX_VALUE, root);
+    }
+    
+    public static boolean inorder(int left, int right, Node root){
+        if(root == null) return false;
+        
+        if(left == right) return true;
+        
+        return inorder(left, root.data - 1, root.left) ||
+        inorder(root.data + 1, right, root.right);
+    }
+}

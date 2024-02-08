07fe5e9edf572ea1008db4105096f853b5f770a9
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2024-02-08 23:33:11 +0530

    Create Solution.java

---
+++ Tree/Check if all leaves are at same level/Solution.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    boolean check(Node root)
+    {
+	    // Your code here
+	    HashSet<Integer> hset = new HashSet<>();
+	    
+	    dfs(root, 0, hset);
+	    
+	    return hset.size() == 1;
+    }
+    
+    void dfs(Node root, int level, HashSet<Integer> hset){
+        if(root == null) return;
+        if(root.left == null && root.right == null) hset.add(level);
+        
+        dfs(root.left, level + 1, hset);
+        dfs(root.right, level + 1, hset);
+    }
+    
+}

9aec63c318fa4bfe0d70e418ad09b3c453aeeded
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2024-06-08 23:51:21 +0530

    Update Solution.java

--- Array/Index of an Extra Element/Solution.java
+++ Array/Index of an Extra Element/Solution.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public int findExtra(int n, int arr1[], int arr2[]) {
+        // add code here.
+        for(int i=0; i<n; i++){
+            if(i == n-1) return i;
+            if(arr1[i] != arr2[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
